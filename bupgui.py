2d0c2fb575437518a14439fa5d3ed6eddf31c855
mess42 <moritz.esslinger@web.de>
2015-01-25 12:20:53 +0100

    a primitive gui for bup

---
+++ bupgui.py
@@ -0,0 +1,55 @@
+#!/usr/bin/env/python
+"""
+bupgui is a primitive gui for the commandline backup bup. 
+
+Copyright (C) 2015 Moritz Esslinger moritz.esslinger@web.de
+
+This program is free software; you can redistribute it and/or
+modify it under the terms of the GNU General Public License
+as published by the Free Software Foundation; either version 2
+of the License, or (at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+"""
+
+
+#include <batteries.h>
+from Tkinter import *
+import sys, os
+import getpass
+
+class bupgui(Frame):
+    def __init__(self, master= None):
+        Frame.__init__(self, master)
+        self.displayinit = False
+        self.pack()
+        self.createWidgets()
+        
+    def createWidgets(self):
+        self.savebutton = Button( self, text="Save /home/"+getpass.getuser(), command=self.bupsave )
+        self.savebutton.grid(row=0, column=0, columnspan=1)
+        self.viewbutton = Button( self, text="View Backups", command=self.viewbackups )
+        self.viewbutton.grid(row=1, column=0, columnspan=1)
+
+    def bupsave(self):
+        os.system( "bup -d bupdir index -u ~/" )
+        os.system( "bup -d bupdir save -n backup_" + getpass.getuser() + " ~/" )
+
+    def viewbackups(self):
+        #fusermount -u 
+        os.system("bup -d bupdir fuse mountpoint")
+        os.system("nautilus " + os.getcwd() + "/mountpoint")
+
+
+
+root = Tk()
+root.title( "simple bup gui" )
+app = bupgui(master = root )
+app.mainloop()
